a9ba19b177f0443f41351bc6a02822818c832cb7
Rubens <rubensluizlobo@gmail.com>
2024-02-20 00:25:39 -0300

    requerimentos cumpridos

--- home/index.js
+++ home/index.js
@@ -1,18 +1,48 @@
 'use strict'
 
-
+let filtro = 0;
 const idPerfil = localStorage.getItem('idusuario')
 if (!idPerfil) {
     window.location.href = './login/login.html'
 }
 
 const tarefaPage = document.getElementById('adicionarTarefaPage')
+const tarefaEditPage = document.getElementById('editarTarefaPage')
 const botaoLogout = document.getElementById('logout')
+const filtroButton = document.getElementById('filtroButton')
 const painelFiltro = document.getElementById('painelFiltro')
-const closePanel = document.getElementById('botaoFecharPainel')
+const closePanelNovaTarefa = document.getElementById('botaoFecharPainelNovaTarefa')
+const closePanelEditarTarefa = document.getElementById('botaoFecharPainelEditarTarefa')
 const mensagemWarning = document.getElementById('mensagemInfoWarning');
+const listaTarefas = document.getElementById('tarefas')
 
 
+
+let painelFiltroOn = false
+
+filtroButton.addEventListener('click',filtrar) 
+function filtrar(){
+    filtro++
+    if(filtro==5){
+        filtro=0
+    }
+    // console.log(filtro)
+    while (listaTarefas.firstChild) {
+        let child = listaTarefas.firstChild
+        listaTarefas.removeChild(child);
+    }
+    criarTarefas(filtro)
+}
+
+// filtroButton.addEventListener('click', () => {
+//     if(!painelFiltroOn){
+//         painelFiltro.style.display = 'block';
+//         painelFiltroOn = true
+//     } else {
+//         painelFiltro.style.display = 'none';
+//         painelFiltroOn = false
+//     }
+// });
 async function validarTarefas() {
     const responseApi = await fetch('http://localhost:5080/tarefas')
     const listTasks = await responseApi.json()
@@ -30,8 +60,8 @@ async function cadastroTarefa() {
 
     const titulo = document.getElementById('tituloTarefaNova').value
     const descricao = document.getElementById('descricaoTarefaNova').value
-    const data = document.querySelector("#dataTarefa").value
-    const inputs = document.querySelectorAll('#categoria input[type="radio"]');
+    const data = document.querySelector("#dataTarefaNova").value
+    const inputs = document.querySelectorAll('#categoriaTarefaNova input[type="radio"]');
 
     let categoria = '';
     for (var i = 0; i < inputs.length; i++) {
@@ -70,26 +100,91 @@ async function cadastroTarefa() {
         }
     }
 }
-function deletarTarefa(){
-    alert("Função em desenvolvimento")
-}
-// async function deletarTarefa(id) {
-//     try {
-//         await fetch(`http://localhost:5080/tarefas/${id}`, {
-//             method: 'DELETE',
-//         });
-//         console.log('Tarefa excluída com sucesso!');
-//     } catch (error) {
-//         console.error('Ocorreu um erro ao excluir a tarefa:', error);
-//     }
-// }
-criarTarefas()
+async function abrirPainelEdicao(idTarefa){
+    const titulo = document.getElementById('tituloTarefaEditada')
+    const descricao = document.getElementById('descricaoTarefaEditada')
+    const data = document.querySelector("#dataTarefaEditada")
+    const inputs = document.querySelectorAll('#categoriaTarefaEditada input[type="radio"]');
 
-async function criarTarefas() {
-    const infoTarefas = await validarTarefas()
-    for (let cont = 0; cont < infoTarefas.length; cont++) {
-        criarTarefa(infoTarefas[cont])
+    const tarefas = await validarTarefas()
+    for(let cont = 0;cont<tarefas.length;cont++){
+        if(idTarefa===tarefas[cont].id){
+            titulo.value = tarefas[cont].titulo
+            descricao.value = tarefas[cont].descricao
+            let dataAtual = tarefas[cont].data
+            data.value = dataAtual
+            console.log(dataAtual)
+            if(inputs[(tarefas[cont].categoria)-1])
+            inputs[(tarefas[cont].categoria)-1].checked = true
+        }
     }
+    tarefaEditPage.style.visibility = 'visible'
+
+    const botaoEditar = document.getElementById('finalizarEdicao')
+    botaoEditar.addEventListener('click', () => editarTarefa(idTarefa));
+}
+
+async function editarTarefa(id) {
+    
+    const titulo = document.getElementById('tituloTarefaEditada').value
+    const descricao = document.getElementById('descricaoTarefaEditada').value
+    const data = document.querySelector("#dataTarefaEditada").value
+    const inputs = document.querySelectorAll('#categoriaTarefaEditada input[type="radio"]');
+    let categoria = '';
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            categoria = inputs[i].value;
+            break; // Interrompe o loop quando encontrar o selecionado
+        }
+    }
+
+    const tarefaAtualizada = {
+        titulo: titulo,
+        descricao: descricao,
+        data: data,
+        categoria: categoria,
+        idUsuario: idPerfil
+    };
+
+    try {
+        await fetch(`http://localhost:5080/tarefas/${id}`, {
+            method: 'PUT', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(tarefaAtualizada)
+        });
+        console.log('Tarefa substituída com sucesso!');
+    } catch (error) {
+        console.error('Ocorreu um erro ao substituir a tarefa: ', error);
+    }
+} 
+
+async function deletarTarefa(id) {
+    try {
+        await fetch(`http://localhost:5080/tarefas/${id}`, {
+            method: 'DELETE',
+        });
+        console.log('Tarefa excluída com sucesso!');
+        window.location.reload()
+    } catch (error) {
+        console.error('Ocorreu um erro ao excluir a tarefa:', error);
+    }
+}
+criarTarefas()
+async function criarTarefas(filtro) {
+    const infoTarefas = await validarTarefas()
+    if(filtro>0){
+        for (let cont = 0; cont < infoTarefas.length; cont++) {
+            if(infoTarefas[cont].categoria===filtro)
+            criarTarefa(infoTarefas[cont])
+        }
+    } else {
+        for (let cont = 0; cont < infoTarefas.length; cont++) {
+            criarTarefa(infoTarefas[cont])
+        }
+    }
+
 }
 botaoLogout.addEventListener('click', logout)
 function logout() {
@@ -97,9 +192,13 @@ function logout() {
     window.location.reload()
 }
 
-closePanel.addEventListener('click', fecharPainel)
-function fecharPainel() {
+closePanelNovaTarefa.addEventListener('click', fecharPainelNovaTarefa)
+function fecharPainelNovaTarefa() {
     tarefaPage.style.visibility = "hidden"
+}
+closePanelEditarTarefa.addEventListener('click', fecharPainelEditarTarefa)
+function fecharPainelEditarTarefa() {
+    tarefaEditPage.style.visibility = "hidden"
 }
 
 function callAdicionarTarefaPage() {
@@ -108,7 +207,6 @@ function callAdicionarTarefaPage() {
 
 
 function criarTarefa(infoTarefas) {
-    const listaTarefas = document.getElementById('tarefas')
    
     const tarefa = document.createElement('div')
     tarefa.classList.add('tarefa')
@@ -141,9 +239,13 @@ function criarTarefa(infoTarefas) {
     date.classList.add('date')
 
     const trashIcon = document.createElement('img')
-    trashIcon.classList.add('trashIcon')
     trashIcon.src = "./img/trashIcon.png"
+    trashIcon.classList.add('imgIcon')
     
+    const editIcon = document.createElement('img')
+    editIcon.src = "./img/editIcon.png"
+    editIcon.classList.add('imgIcon')
+
     const tarefaBottom = document.createElement('div')
     tarefaBottom.classList.add('tarefaBottom')
     tarefaBottom.classList.add(alterarCorBottomTarefa(infoTarefas.categoria))
@@ -157,8 +259,9 @@ function criarTarefa(infoTarefas) {
 
     const categoriaIcon = document.createElement('img')
 
-    categoriaIcon.src = "./img/tarefasIcon/" + verificaCategoria(infoTarefas.categoria) + ".png"
 
+    if(infoTarefas.categoria)
+    categoriaIcon.src = "./img/tarefasIcon/" + verificaCategoria(infoTarefas.categoria) + ".png"
 
 
     tarefaCategoria.appendChild(categoriaIcon)
@@ -169,6 +272,7 @@ function criarTarefa(infoTarefas) {
     if(!infoTarefas.descricao){
         if(infoTarefas.categoria){
             tarefaTopRight.appendChild(tarefaCategoria)
+
         }
         tarefaTop.style.borderRadius = "26px";
     } else {
@@ -185,26 +289,14 @@ function criarTarefa(infoTarefas) {
         }
         tarefaTopRight.appendChild(date)
     }
+    tarefaTopRight.appendChild(editIcon)
     tarefaTopRight.appendChild(trashIcon)
     tarefaTop.appendChild(tituloTarefa)
     tarefaTop.appendChild(tarefaTopRight)
     listaTarefas.appendChild(tarefa)
-    trashIcon.addEventListener('click',deletarTarefa)
-
+    editIcon.addEventListener('click', () => abrirPainelEdicao(infoTarefas.id));
+    trashIcon.addEventListener('click', () => deletarTarefa(infoTarefas.id));
 }
-
-let painelFiltroOn = false
-filtroButton.addEventListener('click', () => {
-    if(!painelFiltroOn){
-        painelFiltro.style.display = 'block';
-        painelFiltroOn = true
-    } else {
-        painelFiltro.style.display = 'none';
-        painelFiltroOn = false
-    }
-});
-
-
 
 function verificaCategoria(categoria) {
     let imgCategoria
